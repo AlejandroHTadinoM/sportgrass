4f136c5ec112f7eda86522d35303a310e0a42b77
alejandrohtadinom <alejandrohtadinom@gmail.com>
2017-06-07 09:44:05 -0400

    Changed: Slider img

--- assets/js/2-slider.js
+++ assets/js/2-slider.js
@@ -5,27 +5,49 @@ function resizeImg () {
 	slideImg.height(sliderContainer.height());
 	slides.width(sliderContainer.width() * slide.length);
 };
+
 $(window).resize(function () {
 	resizeImg();
 });
+
 resizeImg();
+
 function startSlider () {
+
 	timer = setInterval(function () {
+
 		slides.animate({
+
 			'margin-left' : '-=' + sliderContainer.width()
+
 		}, animSpeed, function () {
+
 			cSlide++;
+
 			if (cSlide === slide.length) {
+
 				cSlide = 1;
+
 				slides.css({
+
 					'margin-left' : 0
+
 				});
+
 			};
+
 		}
+
 	)}, 3000);
+
 };
+
 function stopSlider () {
+
 	clearInterval(timer);
+
 };
+
 sliderContainer.mouseenter(stopSlider).mouseleave(startSlider);
+
 startSlider();
